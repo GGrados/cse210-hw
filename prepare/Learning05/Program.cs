30986a58ab29ee3cb45cc95c87aeb6caf9a5021e
Cesar-GH99 <cesarg.grados@gmail.com>
2023-03-01 15:55:13 -0400

    Week 09 prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        Square square = new Square("Yellow", 6);
+        Rectangle rectangle = new Rectangle("Blue", 10, 7);
+        Circle circle = new Circle("Green", 8.5);
+
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
+
     }
 }
